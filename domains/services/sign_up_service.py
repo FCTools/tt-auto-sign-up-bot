ab11e9dc026936452215f9c74db9810e6bdc0015
German Yakimov <german13yakimov@gmail.com>
2020-11-24 16:57:40 +0700

    Fully implement one registration branch.

--- domains/services/sign_up_service.py
+++ domains/services/sign_up_service.py
@@ -2,7 +2,7 @@
 Copyright © 2020 FC Tools. All rights reserved.
 Author: German Yakimov
 """
-
+import random
 import time
 
 from selenium.webdriver.support.ui import WebDriverWait
@@ -66,22 +66,21 @@ class SignUpService(metaclass=Singleton):
     def _solve_screen_1(self, browser, mail, password):
         login_xpath = '//*[@id="TikTokAds_Register"]/section/div[2]/main/form/div[1]/div[2]/div/div/div/input'
         agreement_checkbox_xpath = '//*[@id="TikTokAds_Register"]/section/div[2]/main/form/div[5]/div[1]/div'
-        captcha_xpath = ''
         submit_button_xpath = '//*[@id="TikTokAds_Register"]/section/div[2]/main/form/div[7]/div/button[2]'
         verification_code_xpath_button = '//*[@id="TikTokAds_Register-account-center-code-btn"]'
         verification_code_field_xpath = '//*[@id="TikTokAds_Register"]/section/div[2]/main/form/div[4]/div/div/div[1]/input'
 
-        time.sleep(5)
+        self._random_sleep()
         browser.find_element_by_xpath(login_xpath).send_keys(mail)
-        time.sleep(1)
+        self._random_sleep()
         browser.find_element_by_name("password").send_keys(password)
-        time.sleep(1)
+        self._random_sleep()
         browser.find_element_by_name("repeatPwd").send_keys(password)
-        time.sleep(1)
+        self._random_sleep()
         browser.find_element_by_xpath(agreement_checkbox_xpath).click()
-        time.sleep(2)
+        self._random_sleep()
         browser.find_element_by_xpath(verification_code_xpath_button).click()
-        time.sleep(3)
+        self._random_sleep()
 
         error_xpath = '//*[@id="TikTokAds_Register"]/section/div[2]/main/form/div[6]/div'
         if len(browser.find_elements_by_xpath(error_xpath)) > 0 and \
@@ -91,21 +90,80 @@ class SignUpService(metaclass=Singleton):
         # time.sleep(120)
         verification_code = self._mail_service.find_verification_code(mail, password)
         browser.find_element_by_xpath(verification_code_field_xpath).send_keys(verification_code)
-        time.sleep(2)
+        self._random_sleep()
         browser.find_element_by_xpath(submit_button_xpath).click()
 
         return "OK", browser
 
     def _solve_screen_2(self, browser, country):
-        time.sleep(5)
+        self._random_sleep()
         browser.find_element_by_class_name("vi-input__inner").click()
+        self._random_sleep()
         browser.find_element_by_class_name("vi-input__inner").send_keys(country)
-        time.sleep(1)
+        self._random_sleep()
         browser.find_element_by_class_name("vi-button--primary").click()
 
         return "OK", browser
 
-    def _solve_screen_3(self, browser):
+    def _random_phone_number(self):
+        first = str(random.randint(100, 999))
+        second = str(random.randint(1, 888)).zfill(3)
+        last = (str(random.randint(1, 9998)).zfill(4))
+
+        while last in ['1111', '2222', '3333', '4444', '5555', '6666', '7777', '8888']:
+            last = (str(random.randint(1, 9998)).zfill(4))
+        return f'{first}{second}{last}'
+
+    @staticmethod
+    def _random_sleep():
+        time.sleep(random.randint(1, 5))
+
+    def _solve_screen_3(self, browser, mail, country):
+        country_edit_xpath = '//*[@id="app"]/section/div[1]/section/div/div/section/div[5]/div/section/form/div[1]' \
+                             '/div[1]/div[1]/label/div/span[2]'
+        country_selector_xpath = '//*[@id="app"]/section/div[1]/section/div/div/section/div[5]/div/section/form/' \
+                                 'div[1]/div[1]/div[1]/div/div/div[1]/span/span/i'
+        country_field_xpath = '//*[@id="app"]/section/div[1]/section/div/div/section/div[5]/div/section/form/div[1]/' \
+                              'div[1]/div[1]/div/div/div[1]/input'
+
+        business_name_xpath = '//*[@id="app"]/section/div[1]/section/div/div/section/div[5]/div/section/form/div[1]/' \
+                              'div[1]/div[2]/div/div/input'
+
+        currency_selector_xpath = '//*[@id="app"]/section/div[1]/section/div/div/section/div[5]/div/section/form/' \
+                                  'div[1]/div[1]/div[3]/div/div/div[1]/span/span/i'
+        currency_field_xpath = '//*[@id="app"]/section/div[1]/section/div/div/section/div[5]/div/section/form/div[1]/' \
+                               'div[1]/div[3]/div/div/div[1]/input'
+
+        phone_xpath = '//*[@id="app"]/section/div[1]/section/div/div/section/div[5]/div/section/form/' \
+                      'div[1]/div[2]/div[2]/div/div[1]/input'
+
+        agreement_xpath = '//*[@id="form-item-vw"]/div[1]/div[2]/label/span/span'
+
+        submit_button_xpath = '//*[@id="app"]/section/div[1]/section/div/div/section/div[5]/div/section/' \
+                              'form/div[4]/div/button'
+
+        business_name = mail.split('@')[0]
+        phone_number = self._random_phone_number()
+
+        self._random_sleep()
+        browser.find_element_by_xpath(country_edit_xpath).click()
+        self._random_sleep()
+        browser.find_element_by_xpath(country_selector_xpath).click()
+        self._random_sleep()
+        browser.find_element_by_xpath(country_field_xpath).send_keys(country)
+        time.sleep(5)
+        browser.find_element_by_xpath(business_name_xpath).send_keys(business_name)
+        self._random_sleep()
+        browser.find_element_by_xpath(phone_xpath).send_keys(phone_number)
+        self._random_sleep()
+        browser.find_element_by_xpath(agreement_xpath).click()
+        self._random_sleep()
+        browser.find_element_by_xpath(currency_selector_xpath).click()
+        self._random_sleep()
+        browser.find_element_by_xpath(currency_field_xpath).send_keys("USD")
+        self._random_sleep()
+        browser.find_element_by_xpath(submit_button_xpath).click()
+
         return "OK", browser
 
     def _solve_screen_4(self, browser):
@@ -133,7 +191,7 @@ class SignUpService(metaclass=Singleton):
                 postal_code):
         browser = self._build_browser(proxy)
         browser.get("https://ads.tiktok.com/i18n/signup/")
-        time.sleep(10)
+        time.sleep(15)
 
         if self._detect_screen(browser) == 1:
             status, browser = self._solve_screen_1(browser, mail, password)
@@ -142,14 +200,15 @@ class SignUpService(metaclass=Singleton):
                 browser.close()
                 return status
 
-            status, browser = self._solve_screen_3(browser)
+            status, browser = self._solve_screen_3(browser, mail, country)
+            return status
+
         elif self._detect_screen(browser) == 2:
             status, browser = self._solve_screen_2(browser, country)
             status, browser = self._solve_screen_4(browser)
 
-            if status != "OK":
-                browser.close()
-                return status
+            browser.close()
+            return status
 
         # check inbox here, put it to field and click submit button
 
