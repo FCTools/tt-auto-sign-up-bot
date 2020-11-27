970d47a4a5bf571dd984e4bfc689b7cf80068873
German Yakimov <german13yakimov@gmail.com>
2020-11-27 19:07:52 +0700

    Fix bug with incorrect method name in sign_up_service.py

--- domains/services/sign_up_service.py
+++ domains/services/sign_up_service.py
@@ -235,7 +235,7 @@ class SignUpService(metaclass=Singleton):
         screen_elements = self._screens['screens_elements']['screen_5']
         print(browser.title)
 
-        if len(browser.find_elements_by_xpaths('//*[@id="app"]/section/div[5]/div[2]/div/div/div/div[1]/i')) > 0:
+        if len(browser.find_elements_by_xpath('//*[@id="app"]/section/div[5]/div[2]/div/div/div/div[1]/i')) > 0:
             try:
                 browser = self._click(browser, xpath='//*[@id="app"]/section/div[5]/div[2]/div/div/div/div[1]/i')
                 print("Close spam window.")
